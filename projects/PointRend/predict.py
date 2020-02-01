886674f8278c86fee997f02de674c710bb38f6e2
akashsengupta1997 <as2562@cam.ac.uk>
2020-02-01 16:08:47 +0000

    .

--- projects/PointRend/predict.py
+++ projects/PointRend/predict.py
@@ -79,7 +79,6 @@ def main(args):
     DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
         cfg.MODEL.WEIGHTS, resume=args.resume
     )
-    print(model)
     # res = Trainer.test(cfg, model)
     # if comm.is_main_process():
     #     verify_results(cfg, res)
