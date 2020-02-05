7793198f18cb94574b2ecde2ae77cd64e5cf7d1f
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-02-05 19:24:36 +0000

    trying keypoint rcnn inference

--- projects/DensePose/visualise_from_dump.py
+++ projects/DensePose/visualise_from_dump.py
@@ -83,7 +83,7 @@ def visualise_denspose_results(dump_file, out_folder):
             bbox_index = sorted_bbox_indices[i]
             bbox = bboxes_xyxy[bbox_index]
             bbox_centre = ((bbox[0]+bbox[2])/2.0, (bbox[1]+bbox[3])/2.0)
-            if abs(bbox_centre[0] - orig_w/2.0) < 100 and abs(bbox_centre[1] - orig_h/2.0) < 100:
+            if abs(bbox_centre[0] - orig_w/2.0) < 50 and abs(bbox_centre[1] - orig_h/2.0) < 50:
                 largest_bbox_index = bbox_index
                 bbox_found = True
             i += 1
@@ -93,7 +93,6 @@ def visualise_denspose_results(dump_file, out_folder):
 
         # Round bbox to int
         largest_bbox = bboxes_xyxy[largest_bbox_index]
-        print(largest_bbox)
         w1 = largest_bbox[0]
         w2 = largest_bbox[0] + iuv_arr.shape[2]
         h1 = largest_bbox[1]
---
+++ projects/KeyPointRCNN/inference.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+im = cv2.imread("./input.jpg")
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = v.draw_instance_predictions(outputs["instances"].to("cspu"))
+# cv2.imshow(v.get_image()[:, :, ::-1])
