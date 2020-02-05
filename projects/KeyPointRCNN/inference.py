561b7d03f1b21f3f8cca87be55fa04e51e2056c5
akashsengupta1997 <as2562@cam.ac.uk>
2020-02-05 19:31:58 +0000

    .

--- projects/KeyPointRCNN/inference.py
+++ projects/KeyPointRCNN/inference.py
@@ -9,13 +9,13 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
-im = cv2.imread("./input.jpg")
+im = cv2.imread("/data/cvfs/as2562/datasets/sports_videos_smpl/class_videos/beach_handball/cropped_frames/002/clip_002/person_002/000001.png")
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
-v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-v = v.draw_instance_predictions(outputs["instances"].to("cspu"))
-# cv2.imshow(v.get_image()[:, :, ::-1])
+#v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+#v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+#cv2.imshow(v.get_image()[:, :, ::-1])
