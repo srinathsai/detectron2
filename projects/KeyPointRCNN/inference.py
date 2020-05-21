acd796c0f79d151fb5516adaa8121aa7680382c6
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-05-21 17:29:03 +0100

    testing

--- projects/KeyPointRCNN/inference.py
+++ projects/KeyPointRCNN/inference.py
@@ -56,49 +56,49 @@ def predict_on_folder(in_folder, out_folder, config_file, save_kps_in_separate_f
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_file)
     predictor = DefaultPredictor(cfg)
 
-    os.makedirs(os.path.join(out_folder, 'keypoints_vis'), exist_ok=True)
-    if save_kps_in_separate_files:
-        os.makedirs(os.path.join(out_folder, 'keypoints'), exist_ok=True)
-
-    image_fnames = [f for f in sorted(os.listdir(in_folder)) if f.endswith('.png')]
-    all_keypoints = []
-    for fname in image_fnames:
-        print(fname)
-        image = cv2.imread(os.path.join(in_folder, fname))
-        orig_h, orig_w = image.shape[:2]
-        outputs = predictor(image)
-        bboxes = outputs['instances'].pred_boxes.tensor.cpu().numpy()
-        print(bboxes.shape, bboxes)
-        if bboxes.shape[0] == 0:  # Can't find any people in image
-            keypoints = np.zeros((17, 3))
-            all_keypoints.append(keypoints)
-        else:
-            largest_bbox_index = get_largest_centred_bounding_box(bboxes, orig_w, orig_h)
-            keypoints = outputs['instances'].pred_keypoints.cpu().numpy()
-            keypoints = keypoints[largest_bbox_index]
-
-            if not save_kps_in_separate_files:
-                all_keypoints.append(keypoints)
-            else:
-                print(keypoints.shape)
-                save_keypoints_path = os.path.join(out_folder, 'keypoints', os.path.splitext(fname)[0] + '.npy')
-                np.save(save_keypoints_path, keypoints)
-
-            for j in range(keypoints.shape[0]):
-                cv2.circle(image, (keypoints[j, 0], keypoints[j, 1]), 5, (0, 255, 0), -1)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 0.5
-                fontColor = (0, 0, 255)
-                cv2.putText(image, str(j), (keypoints[j, 0], keypoints[j, 1]),
-                            font, fontScale, fontColor, lineType=2)
-            save_vis_path = os.path.join(out_folder, 'keypoints_vis', fname)
-            cv2.imwrite(save_vis_path, image)
-
-    if not save_kps_in_separate_files:
-        all_keypoints = np.stack(all_keypoints, axis=0)
-        print(all_keypoints.shape)
-        save_keypoints_path = os.path.join(out_folder, 'keypoints.npy')
-        np.save(save_keypoints_path, all_keypoints)
+    # os.makedirs(os.path.join(out_folder, 'keypoints_vis'), exist_ok=True)
+    # if save_kps_in_separate_files:
+    #     os.makedirs(os.path.join(out_folder, 'keypoints'), exist_ok=True)
+    #
+    # image_fnames = [f for f in sorted(os.listdir(in_folder)) if f.endswith('.png')]
+    # all_keypoints = []
+    # for fname in image_fnames:
+    #     print(fname)
+    #     image = cv2.imread(os.path.join(in_folder, fname))
+    #     orig_h, orig_w = image.shape[:2]
+    #     outputs = predictor(image)
+    #     bboxes = outputs['instances'].pred_boxes.tensor.cpu().numpy()
+    #     print(bboxes.shape, bboxes)
+    #     if bboxes.shape[0] == 0:  # Can't find any people in image
+    #         keypoints = np.zeros((17, 3))
+    #         all_keypoints.append(keypoints)
+    #     else:
+    #         largest_bbox_index = get_largest_centred_bounding_box(bboxes, orig_w, orig_h)
+    #         keypoints = outputs['instances'].pred_keypoints.cpu().numpy()
+    #         keypoints = keypoints[largest_bbox_index]
+    #
+    #         if not save_kps_in_separate_files:
+    #             all_keypoints.append(keypoints)
+    #         else:
+    #             print(keypoints.shape)
+    #             save_keypoints_path = os.path.join(out_folder, 'keypoints', os.path.splitext(fname)[0] + '.npy')
+    #             np.save(save_keypoints_path, keypoints)
+    #
+    #         for j in range(keypoints.shape[0]):
+    #             cv2.circle(image, (keypoints[j, 0], keypoints[j, 1]), 5, (0, 255, 0), -1)
+    #             font = cv2.FONT_HERSHEY_SIMPLEX
+    #             fontScale = 0.5
+    #             fontColor = (0, 0, 255)
+    #             cv2.putText(image, str(j), (keypoints[j, 0], keypoints[j, 1]),
+    #                         font, fontScale, fontColor, lineType=2)
+    #         save_vis_path = os.path.join(out_folder, 'keypoints_vis', fname)
+    #         cv2.imwrite(save_vis_path, image)
+    #
+    # if not save_kps_in_separate_files:
+    #     all_keypoints = np.stack(all_keypoints, axis=0)
+    #     print(all_keypoints.shape)
+    #     save_keypoints_path = os.path.join(out_folder, 'keypoints.npy')
+    #     np.save(save_keypoints_path, all_keypoints)
 
 
 if __name__ == '__main__':
