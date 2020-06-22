7d5c202e22b4374556ab60ba2e94824fc7867930
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-06-22 16:01:06 +0100

    Update visualise_from_dump.py

--- projects/DensePose/visualise_from_dump.py
+++ projects/DensePose/visualise_from_dump.py
@@ -126,7 +126,7 @@ def visualise_denspose_results(dump_file, out_folder, save_uv=False, path_correc
             # vis_U_image = vis_U_image[:, :, :3].astype(np.float32)
             # vis_U_image[I_image == 0, :] = np.zeros(3, dtype=np.float32)
             vis_U_image = np.stack([U_image]*3, axis=2)
-            print(vis_U_image.shape, frame.shape)
+            print(vis_U_image.dtype, frame.dtype)
             overlay = cv2.addWeighted(frame,
                                       0.6,
                                       vis_U_image,
